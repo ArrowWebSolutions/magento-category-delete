ad950796e271c7b4dfee36bd6372c36c7d2c0949
Arron King <arron.king@arrowdesign.co.uk>
2012-09-03 16:04:36 +0100

    Added some more options, most notibly you now have to confirm you want to delete the categories via a flag. This is after wiping out all the categories. Also added a warning when the from is not set, this is because it will most likely wipe out the root category. You can either force the script to run or provide a from argument.

--- shell/delete_categories.php
+++ shell/delete_categories.php
@@ -17,45 +17,63 @@ class Mage_Shell_DeleteCategories extends Mage_Shell_Abstract
      */
     public function run()
     {
-        $from = $this->getArg('from');
-        $to = $this->getArg('to');
-        
-        $dryRun = $this->getArg('dry-run');
-        
-        $categories = Mage::getModel('catalog/category')->getCollection();
-        $categories
-                ->addAttributeToSelect('name')
-                ->addAttributeToSelect('id');
-        
-        if ($from && intval($from) == $from)
+        if (!$this->getArg('c'))
         {
-            $categories
-                ->addAttributeToFilter('entity_id', array(
-                    'gteq' => $from
-                ));
-        }
-        
-        if ($to && intval($to) == $to)
-        {
-            $categories
-                ->addAttributeToFilter('entity_id', array(
-                   'lteq' => $to 
-                ));
-        }
-        
-        $categories->load();
-        
-        $count = $categories->count();
-        
-        if ($dryRun)
-        {
-            echo "Categories that would be deleted: {$count}" . PHP_EOL;
+            echo $this->usageHelp();
         }
         else
         {
-            $categories->delete();
-            echo "Deleted {$count} categories." . PHP_EOL;
+            $from = $this->getArg('from');
+            $to = $this->getArg('to');
+            
+            if (!$from || empty($from) || intval($from) <= 2)
+            {
+                $force = $this->getArg('f');
+                if (!$force)
+                {
+                    echo "You have not set a minimum category ID, this will more than likely delete your root category (usually ID 2). You can either set a from or force the script to run." . PHP_EOL;
+                    return;
+                }
+            }
+
+            $dryRun = $this->getArg('dry-run');
+
+            $categories = Mage::getModel('catalog/category')->getCollection();
+            $categories
+                    ->addAttributeToSelect('name')
+                    ->addAttributeToSelect('id');
+
+            if ($from && intval($from) == $from)
+            {
+                $categories
+                    ->addAttributeToFilter('entity_id', array(
+                        'gteq' => $from
+                    ));
+            }
+
+            if ($to && intval($to) == $to)
+            {
+                $categories
+                    ->addAttributeToFilter('entity_id', array(
+                       'lteq' => $to 
+                    ));
+            }
+
+            $categories->load();
+
+            $count = $categories->count();
+
+            if ($dryRun)
+            {
+                echo "Categories that would be deleted: {$count}" . PHP_EOL;
+            }
+            else
+            {
+                $categories->delete();
+                echo "Deleted {$count} categories." . PHP_EOL;
+            }
         }
+        
     }
 
     /**
@@ -71,8 +89,11 @@ Usage:  php -f delete_categories.php -- [options]
   --from [from]     The category id to delete from (inclusive)
   --to [to]         The category id to delete to (inclusive)
   --dry-run         Don't delete, just output how many categories would be delete
+  
+  -c            Confirm that you want to delete
+  -f            Force the script to run, despite the warnings
   -h            Short alias for help
-  help          This help
+  --help          This help
 
 USAGE;
     }
